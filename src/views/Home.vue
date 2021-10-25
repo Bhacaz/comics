0b5ae22f76f0c0edd0a25973487f4d22d284c988
Jean-Francis Bastien <bhacaz@gmail.com>
2021-10-25 19:43:09 -0400

    Starting home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <section class="hero is-warning">
+    <div class="hero-body">
+      <p class="title">Comics</p>
+      <p class="subtitle">Nice design from <em>readcomicsonline.ru</em></p>
+    </div>
+  </section>
+
+  <section class="section">
+    <input
+      class="input is-rounded"
+      type="text"
+      placeholder="Search"
+      v-model="search"
+    />
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      search: null,
+    };
+  },
+  mounted() {
+    this.$watch("search", () => {
+      fetch("https://readcomicsonline.ru/search?query=" + this.search, {
+        headers: { Origin: "https://readcomicsonline.ru" },
+      })
+        .then((res) => res.json())
+        .then((result) => {
+          console.log(result);
+        });
+    });
   },
 };
 </script>
